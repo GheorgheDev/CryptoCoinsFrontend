073feaebf3e6f087b8e8ecc73216e9b54392e7fd
Gheorghe Bucurici <gheorghe.bucurici@babelgroup.com>
2023-04-08 19:26:43 +0200

    Add the first screen layout record

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { RegisterComponent } from './public/register/register/register.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'register', component: RegisterComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
