3a20f4cf9dbc2f2daa41359ed86d0769fc8b1838
Gheorghe Bucurici <gheorghe.bucurici@babelgroup.com>
2023-04-08 19:34:38 +0200

    Add the first screen layout record for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { RegisterComponent } from './public/register/register/register.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './public/login/login/login.component';
 
-const routes: Routes = [{
-  path: 'register', component: RegisterComponent
-}];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
